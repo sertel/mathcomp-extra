719d115277c23ecc7ddfb05148cd57183e22811e
Sebastian Ertel <sebastian.ertel@gmail.com>
2024-04-11 13:51:50 +0200

    final flake

--- flake.nix
+++ flake.nix
@@ -38,52 +38,11 @@
               src = ./.;
             });
 
-        # Indeed all this release stuff does not make sense anymore.
-        # Releases are delt with via the flake.lock.
-        # That is, one revision direclty maps to one configuration!
-        # Once the library is updated then also the flake is updated.
-        # The old configuration is still available via the old revision.
-        # The question is then:
-        # Where would a list of versions be located for a specific library?
-        # Versions can be listed at the website and the flake lock support
-        # version tags instead of commit hashes as well.
-        mkDrv' = { mce_revision } :
-          let
-            release."0.2.0" = {
-              rev = "715b62ab9974542771ddbfecb2ea93fbcb914e6b";
-              deps = {
-                version = "0.2.0";
-                coq = pkgs.coq_8_19;
-                coqPackages = pkgs.coqPackages_8_19.overrideScope
-                  (self: super: {
-                    mathcomp = super.mathcomp.override { version = "2.2.0"; };
-                  });
-              };
-            };
-            release."0.1.0" = {
-              rev = "969b32d07e9bae4e0d932b968efdc03d4a637e91";
-              deps = {
-                version = "0.1.0";
-                coq = pkgs.coq_8_18;
-                coqPackages = pkgs.coqPackages_8_18.overrideScope
-                  (self: super: {
-                    mathcomp = super.mathcomp.override { version = "2.1.0"; };
-                  });
-              };
-            };
-            deps = with pkgs.coqPackages.lib; switch mce_revision [
-              { case = release."0.2.0".rev; out = release."0.2.0".deps; }
-              { case = release."0.1.0".rev; out = release."0.1.0".deps; }
-            ] (throw "no such version: ${mce_revision}");
-          in
-            mkDrv (with deps; { inherit coq coqPackages version; } );
-
-      in { inherit mkDrv mkDrv'; } //
+      in { inherit mkDrv; } //
          rec {
            devShell =
              let
                args = {
-                 #inherit (pkgs) stdenv which;
                  coqPackages = pkgs.coqPackages_8_18.overrideScope
                    (self: super: {
                      mathcomp = super.mathcomp.override { version = "2.1.0"; };
